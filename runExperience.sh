b05328df1f589c6089fa495296a41d0c4919e437
zaneck <val.owczarek@gmail.com>
2016-06-14 09:15:15 +0200

    auto script

---
+++ runExperience.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -r images
+mkdir images
+mkdir images/circle
+mkdir images/square
+mkdir images/cross
+mkdir images/foursquare
+
+python3 main.py 1
+python3 main.py 2
+python3 main.py 3
+python3 main.py 4
