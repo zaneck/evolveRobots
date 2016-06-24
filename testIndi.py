d39c9daa568a3213485287f0466b7b0339131bc4
Damien Marchal <damien.marchal@univ-lille1.fr>
2016-06-24 15:32:20 +0200

    Extra test

--- testIndi.py
+++ testIndi.py
@@ -34,53 +34,28 @@ class TestIndie(unittest.TestCase):
         with self.assertRaises(MemoryError):
                a = Indi(100000000, 1000000000) 
                 
+    def test_basic_behavior(self):
+                a = Indi(16, 16)
+                b = Indi(16, 16)
+
+                a.addRandomSquare()
+                a.addRandomSquare()
+
+                b.addRandomSquare()
+                b.addRandomSquare()
+                b.addRandomSquare()
+
+                print(a.__dict__)
+                print(b.__dict__)
+
+                c, d = a.crossOver(b)
+
+                print(c.__dict__)
+                print(d.__dict__)
+
+                mat = a.toMatrice()
+              
         
 if __name__ == '__main__':
     unittest.main()
-    
-import sys
-sys.exit(0)
-    
-def fakeIndiCircle(x,y):
-    a = Indi(16,16)
 
-    circleMat = circle(16,16)
-
-    for i in range(16):
-        for j in range(16):
-            if circleMat[i][j] == 1:
-                a.draw.append((i,j,0))    
-    return a
-        
-a = Indi(16, 16)
-b = Indi(16, 16)
-
-a.addRandomSquare()
-a.addRandomSquare()
-
-b.addRandomSquare()
-b.addRandomSquare()
-b.addRandomSquare()
-
-print(a.__dict__)
-print(b.__dict__)
-
-c, d = a.crossOver(b)
-
-print(c.__dict__)
-print(d.__dict__)
-
-mat = a.toMatrice()
-
-f = FitnessCircle()
-
-fakeOne = fakeIndiCircle(16,16)
-
-mat = fakeOne.toMatrice()
-
-printMatrix(mat, 16,16)
-
-value = f.computeValue(fakeOne)
-print(value)
-
-assert value == 0
