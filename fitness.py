0cd209e67ac600d80046dd6e947fbe49789e1f11
zaneck <val.owczarek@gmail.com>
2016-06-24 17:14:52 +0200

    refacto fitness and main

--- fitness.py
+++ fitness.py
@@ -16,116 +16,56 @@ class Fitness:
         
         if self.bestOverAll == None:
             self.bestOverAll = n
-            print("New record {0}".format(self.bestOverAll.fitness))
+#            print("New record {0}".format(self.bestOverAll.fitness))
 
         
         #save the best
         if self.bestOverAll.fitness > fit:
-            print("")
-            imgTest = n.toMatrice()
-            printMatrix(imgTest, self.x, self.y)
+#            print("")
+            # imgTest = n.toMatrice()
+            # printMatrix(imgTest, self.x, self.y)
 
             self.bestOverAll = n
 
-            print("New record {0}".format(self.bestOverAll.fitness))
+#            print("New record {0}".format(self.bestOverAll.fitness))
 
         return fit
 
     def simulate(self, n):
         raise NotImplementedError
 
-class FitnessSquare(Fitness):
-    def __init__(self):
+
+class FitnessImage(Fitness):
+    def __init__(self, name, draw, metric, x, y):
         Fitness.__init__(self)
-        self.name="Square"
+        self.name=name
         
-        self.x = 64
-        self.y = 64
+        self.x = x
+        self.y = y
+
+        self.metric = metric
         
-        self.img = square(self.x, self.y, int(self.x/2), int(self.y/2), color=1)
+        self.img = draw
         self.matriceImg = matriceTocouple(self.img, self.x, self.y)
-        printPict(self.img, self.x, self.y)
-        
-        
+#        printPict(self.img, self.x, self.y)
+              
     def simulate(self, n):
         imgTest = n.toMatrice()
 
-        cptOk = fitnessP(imgTest, self.img, self.x, self.y)
-        #cptOk = fitnessP(imgTest, self.matriceImg, self.x, self.y)
-        
-        return cptOk
-
-class FitnessFourSquare(Fitness):
-    def __init__(self):
-        Fitness.__init__(self)
-        self.name="FourSquare"
-        
-        self.x = 64
-        self.y = 64
-        
-        self.img = fourSquare(self.x, self.y, color=1)
-        self.matriceImg = matriceTocouple(self.img, self.x, self.y)
-        printPict(self.img, self.x, self.y)
-        
-        
-    def simulate(self, n):
-        imgTest = n.toMatrice()
-
-        cptOk = fitnessP(imgTest, self.img, self.x, self.y)
-        #cptOk = fitnessP(imgTest, self.matriceImg, self.x, self.y)
-        
+        if self.metric == 1:
+            cptOk = fitnessHausdorff(imgTest, self.matriceImg, self.x, self.y)
+        elif self.metric == 2:
+            cptOk = fitnessHausdorffAverage(imgTest, self.matriceImg, self.x, self.y)
+        elif self.metric == 3:
+            cptOk = fitnessSorensenDice(imgTest, self.img, self.x, self.y)
+        else:
+            cptOk = fitnessMaxRessemblance(imgTest, self.img, self.x, self.y)
+            
         return cptOk
 
 
-class FitnessCross(Fitness):
-    def __init__(self):
-        Fitness.__init__(self)
-        self.name="Cross"
-
-        self.x = 64
-        self.y = 64
-        
-        self.img = cross(self.x, self.y, int(self.x/2), int(self.y/2), color=1)
-        self.matriceImg = matriceTocouple(self.img, self.x, self.y)
-        printPict(self.img, self.x, self.y)
-        
-        
-    def simulate(self, n):
-        imgTest = n.toMatrice()
-
-        cptOk = fitnessP(imgTest, self.img, self.x, self.y)
-        #cptOk = fitnessP(imgTest, self.matriceImg, self.x, self.y)
-        
-        return cptOk
-
-    
-
-class FitnessCircle(Fitness):
-    def __init__(self):
-        Fitness.__init__(self)
-        self.name="Circle"
-        
-        self.x = 64
-        self.y = 64
-
-        self.img = circle(self.x, self.y, color=1)
-        self.matriceImg = matriceTocouple(self.img, self.x, self.y)
-        printPict(self.img, self.x, self.y)
-        
-        
-    def simulate(self, n):
-        imgTest = n.toMatrice()
-
-        cptOk = fitnessP(imgTest, self.img, self.x, self.y)
-        #cptOk = fitnessP(imgTest, self.matriceImg, self.x, self.y)
-        
-        return cptOk
-
-
-    
 def dist(a,b):
         return math.sqrt(math.pow((a[0]-b[0]) ,2)+pow((a[1]-b[1]) ,2))
-
         
 def hausdorff(A,B):
     try:
@@ -185,25 +125,24 @@ def sorensenDice(img, mimg, x, y):
 
 
     
-# def fitnessP(imgTest, mimg, x, y):
-#     mtest = matriceTocouple(imgTest, x, y)
+def fitnessHausdorff(imgTest, mimg, x, y):
+    mtest = matriceTocouple(imgTest, x, y)
+    return hausdorff(mtest,mimg)
 
-#     return hausdorff(mtest,mimg)
+def fitnessHausdorffAverage(imgTest, mimg, x, y):
+    mtest = matriceTocouple(imgTest, x, y)
 
-# def fitnessP(imgTest, mimg, x, y):
-#     mtest = matriceTocouple(imgTest, x, y)
+    return hausdorffAverage(mtest,mimg)
 
-#     return hausdorffAverage(mtest,mimg)
-
-def fitnessP(imgTest, img, x, y):
+def fitnessSorensenDice(imgTest, img, x, y):
     return sorensenDice(imgTest, img, x, y)
 
 
-# def fitnessP(imgTest, img, x, y):
-#     cpt=0
+def fitnessMaxRessemblance(imgTest, img, x, y):
+    cpt=0
 
-#     for i in range(x):
-#         for j in range(y):                
-#             if img[i][j] != imgTest[i][j]:
-#                 cpt += 1
-#     return cpt /(x*y)
+    for i in range(x):
+        for j in range(y):                
+            if img[i][j] != imgTest[i][j]:
+                cpt += 1
+    return cpt /(x*y)
--- main.py
+++ main.py
@@ -9,39 +9,51 @@ from indi import *
 from genetic import *
 from fitness import *
 
-x, y = 64, 64
+sizex, sizey = 64, 64
 
-p = Population()
+imageDict = {
+    "1":("circle", circle(sizex, sizey)),
+    "2":("square",square(sizex, sizey, int(sizex/2), int(sizey/2))),
+    "3":("fourSquare",fourSquare(sizex, sizey)),
+    "4":("cross",cross(sizex, sizey, int(sizex/2), int(sizey/2))),
+}
 
-fitness = {
-    "1":FitnessCircle,
-    "2":FitnessSquare,
-    "3":FitnessCross,
-    "4":FitnessFourSquare,
+metricDict = {
+    "1":("hausdorff"),
+    "2":("hausdorffAverage"),
+    "3":("SorensenDice"),
+    "4":("maxRessemblance"),
 }
 
 # todo(damien): Il faut une vrai gestion des arguments et des messages d'erreurs. 
-f = fitness[sys.argv[1]]()
+
+shape = sys.argv[1]
+shapeName = imageDict[shape][0]
+shapeMatrix = imageDict[shape][1]
+
+metric = sys.argv[2]
+
+f = FitnessImage(shapeName, shapeMatrix, metric, sizex, sizey)
+
+#todo(valentin): Gerer tous les parametres de l'algo, population, cycleMax... 
+p = Population()
 
 for _ in range(100):
-    a = Indi(x,y)
+    a = Indi(sizex, sizey)
     a.addRandomSquare()
     f.computeValue(a)
     p.addIndi(a)
 
-print(p.numberOfIndi)
-
 g = GeneticAlgo(f, p)
 
-print(g.__dict__)
 
 for alpha in range(100):
-    print("evolve {0}".format(alpha))
+    print("evolve {0}/{1}\r".format(alpha, 100), end="")
     g.evolve()
-
+print("")
 
 best = f.bestOverAll
 
 print("")
 imgTest = best.toMatrice()
-printMatrix(imgTest, x, y)
+printMatrix(imgTest, sizex, sizey)
