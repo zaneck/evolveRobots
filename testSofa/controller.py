80860064ef1acdfff255ae332757bb963807419a
zaneck <val.owczarek@gmail.com>
2016-06-28 15:34:52 +0200

    patch

--- testSofa/controller.py
+++ testSofa/controller.py
@@ -5,15 +5,17 @@ class controller(Sofa.PythonScriptController):
         self.pos =[]
         #20 45 0 30 50 10
         #find the box
-        n=node.getObject('tetras').position
-        for i in range(len(n)):
-            if n[i][0]>20 and n[i][0]<45 and n[i][1]>30 and n[i][1]<50:
+        self.tetras=node.getObject('tetras').position
+
+        for i in range(len(self.tetras)):
+            if self.tetras[i][0]>=20 and self.tetras[i][0]<=30 and  self.tetras[i][1]>=45 and self.tetras[i][1]<=50:
                     self.pos.append(i)
             
 
+    #print distance from init position
     def onEndAnimationStep(self,deltaTime):
         tetras = self.node.getObject('tetras').position
         
         print("animation")
         for i in self.pos:
-            print(tetras[i])
+            print(self.tetras[i][1] - tetras[i][1])
