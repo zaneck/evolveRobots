539b76270eb604b682e3cf217e63949d37bc0cdb
Damien Marchal <damien.marchal@univ-lille1.fr>
2016-07-01 16:33:21 +0200

    ajout de commentaires et question

--- config.py
+++ config.py
@@ -3,15 +3,21 @@ class Config(object):
     generalX, generalY = 16, 16 #size of the grid
 
     #Indi
+
+    #todo(valentin): il faut expliquer c'est quoi ? est ce que size == le nombre ?  
     indiSizeMax = 1024
+
+    #todo(valentin): je ne comprend pas ce que c'est
     indiSquareMaxSize = 1#int(max(generalX, generalX) / 5)
     
     #Population
     PopulationPopMax = 100
 
     #Genetic Algo
+    #todo(valentin): ici expliquer c'est quoi ce param. 
     geneticBest = 10
 
+    #todo(valentin): french vs english.  
     genticNbAugmentation = 100
     genticRatioAdd = 0.10
     genticRatioCross = 0.80
--- mainSofa.py
+++ mainSofa.py
@@ -1,29 +1,36 @@
-#!/bin/python -O
+#!/bin/python3 -O
 #############################################################################
+#
+# This application creates soft-robot designs that match a given specification. 
+# To generates the designs a genetic algorithm is implemented. You can change 
+# the parameters of the genetic algorithm by editting the file [config.py].
+#
 # Contributors:
 #	- created by Valentin Owczarek
 #############################################################################
 import sys
 import argparse
 
-
 from config import Config
 from indi import *
 from genetic import *
 from sofaBaseFitness import *
 
-parser = argparse.ArgumentParser(description='try to create a softRobot by genetic algorithm')
+parser = argparse.ArgumentParser(description="""
+This application creates soft-robot designs that match a given specification. To generates the designs a genetic algorithm
+is implemented. To change the parameters of the genetic algorithm you need to edit the file [config.py]
+""")
 
 #toDo(valentin) add scene and mintopo var
-
 args = parser.parse_args()
 
+######################## Read the configuration file an initialize the algorithm ###########################
 sizex, sizey = Config.generalX, Config.generalY
 
 f = FitnessSofa("sofa")
-
 p = Population()
 
+#### Randomly creates an initial population composed of 'Config.evolveFirstGen' candidates. 
 for alpha in range(Config.evolveFirstGen):
     print("init {0}/{1}\r".format(alpha+1, Config.evolveFirstGen), end="")
     a = Indi(sizex, sizey)
@@ -31,15 +38,15 @@ for alpha in range(Config.evolveFirstGen):
     f.computeValue(a)
     p.addIndi(a)
 
+#### Create the algorithm and do the iterations 
 g = GeneticAlgo(f, p)
-
 print("")
-
 for alpha in range(Config.evolveNbCycle):
     print("evolve {0}/{1}\r".format(alpha+1, Config.evolveNbCycle), end="")
     g.evolve()
 print("")
 
+#### Dup the results.
 best = f.bestOverAll
 
 print("")
