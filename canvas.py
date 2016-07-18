76ee0864695ffadce2aa842b854f3bce3ddd1d3e
zaneck <val.owczarek@gmail.com>
2016-07-18 22:13:49 +0200

    some issue

--- canvas.py
+++ canvas.py
@@ -7,6 +7,7 @@
 #	- created by Valentin Owczarek
 #       - damien.marchal@univ.lille1.fr
 #############################################################################
+import math
 
 class Canvas(object):
     def __init__(self, indi, x, y):
@@ -34,26 +35,21 @@ class CanvasReflectionSymetry(Canvas):
     def __init__(self, indi, x, y):
         Canvas.__init__(self, indi, x, y)
 
-        self.xMin = int(self.x / 2) 
-        self.yMin = int(self.y / 2)
+        self.xMin = math.floor(self.x / 2) 
+        self.yMin = math.floor(self.y / 2)
         
     def toMatrice(self):
         res =[[0 for _ in range(self.y)] for _ in range(self.x)]
             
         for d in self.indi.draw:
             centX, centY, radius = d
-            for i in range(centX - radius, centX + radius+1):
-                for j in range(centY - radius, centY + radius+1):
+            for i in range(centX - radius, centX + radius):
+                for j in range(centY - radius, centY + radius):
                     if i >=0 and i< self.x and j >=0 and j< self.y:
                         res[i][j]=1
 
-            # for i in range((self.x-centX) - radius, (self.x-centX) + radius+1):
-            #     for j in range(centY - radius, centY + radius+1):
-            #         if i >=0 and i< self.x and j >=0 and j< self.y:
-            #             res[i][j]=1
-
-            for i in range(centX - radius, centX + radius+1):
-                for j in range((self.y-centY) - radius, (self.y-centY) + radius+1):
+            for i in range(centX - radius, centX + radius):
+                for j in range((self.y-centY) - radius, (self.y-centY) + radius):
                     if i >=0 and i< self.x and j >=0 and j< self.y:
                         res[i][j]=1
 
@@ -62,4 +58,4 @@ class CanvasReflectionSymetry(Canvas):
         return res
 
     def getMaxXY(self):
-        return (self.xMin, self.yMin)
+        return (self.x, self.yMin)
