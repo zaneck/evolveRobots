4747b5ddf5371241f50a4c1d6973d5e27e7c05eb
zaneck <val.owczarek@gmail.com>
2016-07-22 15:16:27 +0200

    wip

--- genSofaScene/makeScene.py
+++ genSofaScene/makeScene.py
@@ -3,23 +3,32 @@ import sys
 
 import sofaObjectUsable
 import sofaObject
+from organisor import *
 
 f = open("test1.json")
 
 res = json.load(f)
 f.close()
 
+orga = Organisor()
+
 for a in res.keys():
-    cls = getattr(sofaObject, a)
+    cls = getattr(sofaObjectUsable, a)
 
     if cls.unique == False:
         for context in res[a]:
             a = cls(context)
+            orga.addToStock(a)
     else:
         a=cls(res[a])
+        orga.addToStock(a)
 
-    
-    
+pyscn = printPyscnFile(orga, ".")
+minTopo = printMintopoFile(orga, ".")
+
+pyscn.printFile()
+minTopo.printFile()
+
 # head = Head()
 # canvas = Canvas(50,50,5,20,20,1)
 # fem = FEM()
@@ -31,12 +40,16 @@ for a in res.keys():
 # tail = Tail()
 
 # print(head)
+
 #print(canvas)
+
 # print(fem)
+
 # print(partialConstraint)
 # print(fixedConstraint1)
 # print(fixedConstraint2)
 # print(constantForceField)
+
 # print(tail)
 
 # a=["x":50, "y":50, "z":5, "divX":20, "divY":20, "divZ":1]
