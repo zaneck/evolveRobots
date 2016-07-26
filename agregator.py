8e39d3127fad3b31dd7fad139f39b3ca89197776
zaneck <val.owczarek@gmail.com>
2016-07-26 15:59:17 +0200

    agregator file

---
+++ agregator.py
@@ -0,0 +1,14 @@
+# coding: utf8 
+#############################################################################
+#
+# This file is part of evolveRobot. 
+#
+# Contributors:
+#	- created by Valentin Owczarek
+#############################################################################
+
+def maxAgregator(l):
+    return max(l)
+
+def minAgregator(l):
+    return min(l)
