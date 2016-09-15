c3cb38d648672334584c64c1f2b019e6aa6eac75
zaneck <val.owczarek@gmail.com>
2016-09-15 15:30:15 +0200

    add a cache on sofaBase to speedUp the fitness function

--- sofaBaseFitness.py
+++ sofaBaseFitness.py
@@ -8,6 +8,7 @@
 #############################################################################
 import threading
 import os
+import hashlib
 
 from subprocess import Popen, PIPE, call
 
@@ -18,12 +19,38 @@ from config import Config
 
 from agregator import *
 
+
 def binarybin(values):
         """This binning function is using the first value at [0]. If < 0 it return 1.0 (there is matter at
            this location. Otherwise it return 0 (no matter)"""
         if values[0] < 0:
                 return 1
         return 0
+
+def computeKey(m):
+        res = ""
+        for i in range(len(m)):
+                for j in range(len(m[0])):
+                        res+="{0}".format(m[i][j])
+        m= hashlib.md5()
+        m.update(res.encode())
+        hashcode = m.hexdigest()
+        return hashcode
+                        
+def addCache(k, s, raw, table):
+        if k in table:
+                testRaw = table[k][1]
+
+                for i in range(len(testRaw)):
+                        for j in range(len(testRaw[0])):
+                                if testRaw[i][j] != raw[i][j]:
+                                        print("ERROR")
+                                        raise NotImplementedError
+        else:
+                table[k]=(s,raw)
+        
+        return 1
+
 
 class FitnessSofa(Fitness):
         def __init__(self, name, theCanvas,
@@ -47,7 +74,14 @@ class FitnessSofa(Fitness):
                         if t != mainThread:
                                 Popen(["mkdir", "-p", "/tmp/evolveRobots/{0}".format(t.ident)])
                                 Popen(["cp", Config.fitnessSofaSceneFolder + "/controller.py", Config.fitnessSofaSceneFolder + "/"+sofaScene, Config.fitnessSofaSceneFolder + "/tools.py", "/tmp/evolveRobots/{0}".format(t.ident)]) #copy object
-    
+
+                ##############
+                # CACHE INIT #
+                ##############
+                self.cacheLock = threading.Lock()
+                self.cache = {}
+                
+                
         def toMatrice(self, candidate):
                 return self.canvas.toMatrice(candidate, binarybin)
 
@@ -89,7 +123,7 @@ class FitnessSofa(Fitness):
                         for i in tSplit:#for all value
                                 try:
                                         posUnit = float(i)
-                                        if posUnit >= 50:
+                                        if posUnit >= 70:
                                                 return [sys.maxsize]
                                         pos.append(posUnit)
                                 except ValueError:
@@ -105,7 +139,7 @@ class FitnessSofa(Fitness):
                 #write the candidate shape into a file. This file is then read by the python script used to 
                 # load the sofa scene.
                 topo = open(basedir+"topo.py","w") 
-                topo.write("#Topology for candidate #{0}\n".format(candidate.myId))
+                topo.write("#Topology for candidate #{0} score={1}\n".format(candidate.myId, candidate.fitness))
                 topo.write("topology = [\n")
 
                 # Into the shape description file we are writing the image generated from the 
@@ -127,31 +161,56 @@ class FitnessSofa(Fitness):
                 self.writeCandidateMatrix(filename, self.bestOverAll, imgTest)
                 
         def simulate(self, candidate):
-            idThread = threading.get_ident()
-            imgTest, cptVoxel = self.makeCandidateMatrix(candidate)
+                idThread = threading.get_ident()
+                imgTest, cptVoxel = self.makeCandidateMatrix(candidate)
             
-            basedir = "/tmp/evolveRobots/{0}/".format(idThread)
-            self.writeCandidateMatrix(basedir, candidate, imgTest)
+                basedir = "/tmp/evolveRobots/{0}/".format(idThread)
 
-            # In order to score the candidate we need to benchmark it using a sofa simulation
-            # the following line is starting sofa as an external application. Sofa is started 
-            # in batch mode (-g batch) and will do Config.fitnessTimeStep iterations (-n).  
-            # At each iteration step the score will be printed to the standard output 
-            a = Popen(["runSofa", "-g", "batch", "-n", "{0}".format(Config.fitnessTimeStep), "/tmp/evolveRobots/{0}/{1}".format(idThread, self.sofaScene)], stdout=PIPE, universal_newlines=True) #add /tmp/thread.ident
-            astdout, _ = a.communicate()
-            a.stdout.close()
-
-            pos = self.parseResult(astdout)
-
-            if Config.fitnessAgregator == "max":
-                    res = maxAgregator(pos)
-            elif Config.fitnessAgregator == "min":
-                    res = minAgregator(pos)
-            elif Config.fitnessAgregator == "average":
-                    res = averageAgregator(pos)
-            else:
-                    raise ValueError
+                #########
+                # CACHE #
+                #########
+                key = computeKey(imgTest)
+                fitScore = None
+                with self.cacheLock:
+                        if key in self.cache:
+                                fitScore = self.cache[key]
         
+                if fitScore != None:
+                        #     for i in range(len(imgTest)):
+                        #             for j in range(len(imgTest[0])):
+                        #                     if imgTest[i][j] != fitScore[1][i][j]:
+                        #                             print("ERROR")
+                        #                            raise NotImplementedError
+                        return fitScore[0]
+            
+                # In order to score the candidate we need to benchmark it using a sofa simulation
+                # the following line is starting sofa as an external application. Sofa is started 
+                # in batch mode (-g batch) and will do Config.fitnessTimeStep iterations (-n).  
+                # At each iteration step the score will be printed to the standard output 
+                self.writeCandidateMatrix(basedir, candidate, imgTest)
+                a = Popen(["runSofa", "-g", "batch", "-n", "{0}".format(Config.fitnessTimeStep), "/tmp/evolveRobots/{0}/{1}".format(idThread, self.sofaScene)], stdout=PIPE, universal_newlines=True) #add /tmp/thread.ident
+                astdout, _ = a.communicate()
+                a.stdout.close()
+
+                pos = self.parseResult(astdout)
+
+                #print(pos)
+            
+                if Config.fitnessAgregator == "max":
+                        res = maxAgregator(pos)
+                elif Config.fitnessAgregator == "min":
+                        res = minAgregator(pos)
+                elif Config.fitnessAgregator == "average":
+                        res = averageAgregator(pos)
+                else:
+                        raise ValueError
+
+                final = (res * Config.fitnessRateScore) + (cptVoxel * Config.fitnessRateVoxel)
+                #########
+                # CACHE #
+                #########
+                with self.cacheLock:
+                        addCache(key, final, imgTest, self.cache)
                     
-        #TODO valentin : moyenne des carré
-            return (res * Config.fitnessRateScore) + (cptVoxel * Config.fitnessRateVoxel)
+                #TODO valentin : moyenne des carré
+                return final
